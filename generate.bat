8e4ae026d8f5e7e500fbc0f6cb19b3ffaa461ecc
andycee <sinergie@yandex.ru>
2015-11-12 14:35:39 +0200

    Добавил
    Поправил пути в генераторе и обновил информацию по ключам

--- generate.bat
+++ generate.bat
@@ -3,8 +3,8 @@ net use M: \\%~1\C$ 1> null
 echo remote root filesystem successfully connected...
 cd C:\pstools\
 copy key\keyfinder.* m:\
-psexec \\%~1 C:\keyfinder.exe /save /close
-move m:\wka*.txt c:\pstools\osinfo\
+psexec \\%~1 C:\key\keyfinder.exe /save /close
+move m:\wka*.txt C:\Code\admin-tools\osinfo
 del m:\keyfinder*
 net use m: /delete /y 1> null
 echo remote root filesystem successfully disconnected...
